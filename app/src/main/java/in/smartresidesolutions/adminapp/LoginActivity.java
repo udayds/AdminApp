c73e5784a45859b40612f303ecc4b589f25c7d68
udayds <33685812+udayds@users.noreply.github.com>
2018-11-13 20:09:26 +0530

    commit with navigation drawer

---
+++ app/src/main/java/in/smartresidesolutions/adminapp/LoginActivity.java
@@ -0,0 +1,56 @@
+package in.smartresidesolutions.adminapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class LoginActivity extends AppCompatActivity {
+    private EditText mEmailView;
+    private EditText mPasswordView;
+    private View mProgressView;
+    private View mLoginFormView;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+        mEmailView = (EditText) findViewById(R.id.email);
+
+
+        mPasswordView = (EditText) findViewById(R.id.password);
+        mPasswordView.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int id, KeyEvent keyEvent) {
+                if (id == EditorInfo.IME_ACTION_DONE || id == EditorInfo.IME_NULL) {
+
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        Button mEmailSignInButton = (Button) findViewById(R.id.email_sign_in_button);
+        mEmailSignInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityChangeIntent = new Intent(view.getContext(),Navigation.class);
+                startActivity(activityChangeIntent);
+            }
+        });
+
+     /*   Button contact_button = (Button) findViewById(R.id.contact_button);
+        contact_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent activityChangeIntent = new Intent(view.getContext(),LoginContactUs.class);
+                startActivity(activityChangeIntent);
+            }
+        });
+        */
+    }
+}
